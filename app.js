702e82f0ccb9affa1a514337a6ff99c42746d801
lisavncnt <vincent.lisa@orange.fr>
2021-03-17 12:13:22 +0100

    testing user files ok

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://Lisa:pekocko@cluster0.bxvax.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
-mongoose.connect('mongodb+srv://Lisa:pekocko@cluster0.bxvax.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-{ useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connexion à MongoDB Atlas réussie !'))
-.catch(() => console.log('Connexion à MongoDB Atlas échouée !'));
-
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
-app.user('/api/auth', userRoutes);
+app.use(bodyParser.json());
+
+app.use('/api/sauce', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
 
 module.exports = app;
---
+++ routes/sauce.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+
+const sauceCtrl = require('../controllers/sauce');
+
+module.exports = router;
